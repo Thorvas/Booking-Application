974b08ed6b4f40218fd4e92827aba62f1e3874d8
Thorvas <cygan.kacper@outlook.com>
2024-05-28 23:03:47 +0200

    [FEATURE] docker-compose.yml + PostgreSQL database connection added

--- booking/src/main/java/com/booking/booking/booking/BookingModel.java
+++ booking/src/main/java/com/booking/booking/booking/BookingModel.java
@@ -1,4 +1,14 @@
 package com.booking.booking.booking;
 
+import jakarta.persistence.*;
+
+@Entity
 public class BookingModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String date;
+
 }
--- booking/src/main/java/com/booking/booking/booking/BookingRepository.java
+++ booking/src/main/java/com/booking/booking/booking/BookingRepository.java
@@ -1,4 +1,6 @@
 package com.booking.booking.booking;
 
-public class BookingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<BookingModel, Long> {
 }
